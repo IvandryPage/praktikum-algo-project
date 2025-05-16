ad1a2d1d08e4bfa045387ef9a82ce1a79068c842
RafidH777 <124240060@student.upnyk.ac.id>
2025-05-16 21:25:13 +0700

    Membuat tampilan menu yang lain

--- main.cpp
+++ main.cpp
@@ -36,38 +36,80 @@ class ConsoleUI {
     #endif
   }
 
+  static void buatPlaylist(){
+     std::string namaPlaylist;
+
+    std::cout << "\n=====================================\n";
+    std::cout << "          BUAT PLAYLIST BARU      \n";
+    std::cout << "=====================================\n";
+    std::cout << "\n Masukkan nama playlist baru mu: ";
+
+    std::getline(std::cin >> std::ws, namaPlaylist); // Agar bisa input dengan spasi
+
+    std::cout << "\n Playlist " << namaPlaylist <<  " berhasil dibuat!\n";
+    std::cout << "Saatnya mengisi playlist ini dengan lagu favoritmu!\n";
+    std::cout << "-------------------------------------\n\n";
+  }
+
+  static void lihatPlaylist(){
+    std::cout << "\n========================================\n";
+    std::cout << "            DAFTAR PLAYLIST           \n";
+    std::cout << "========================================\n";
+  }
+  static void daftarLagu(){
+    std::cout << "\n===========================================\n";
+    std::cout << "               DAFTAR LAGU               \n";
+    std::cout << "===========================================\n";
+  }
+
+  static void searchingLagu(){
+    std::cout << "\n=====================================\n";
+    std::cout << "           CARI LAGU               \n";
+    std::cout << "=====================================\n";
+    std::cout << "Masukkan judul lagu: ";
+    std::string judul;
+    std::cin.ignore();
+    std::getline(std::cin, judul);
+  }
+
   static void mainMenu() {
-    std::cout << "1. Buat Playlist\n";
-    std::cout << "2. Lihat Playlist\n";
-    std::cout << "3. Daftar Lagu\n";
-    std::cout << "4. Cari Lagu\n";
-    std::cout << "0. Keluar\n";
-    std::cout << "Pilihan Menu : ";
+    std::cout << "=====================================\n";
+    std::cout << "              RAiVFY              \n";
+    std::cout << "=====================================\n";
+    std::cout << " 1.  Buat Playlist\n";
+    std::cout << " 2.  Lihat Playlist\n";
+    std::cout << " 3.  Daftar Lagu\n";
+    std::cout << " 4.  Cari Lagu\n";
+    std::cout << " 0.  Keluar\n";
+    std::cout << "-------------------------------------\n";
+    std::cout << " Pilihan Menu : ";
+
     int pilihan;
     std::cin >> pilihan;
+
     switch (pilihan)
     {
-    case 1:
-      
-      break;
-    case 2:
-      
-      break;
-    case 3:
-      
-      break;
-    case 4:
-      
-      break;
-    case 0:
-      
-      break;
-    
-    default:
-      break;
+        case 1: buatPlaylist();
+            // Buat Playlist
+            break;
+        case 2: lihatPlaylist();
+            // Lihat Playlist
+            break;
+        case 3: daftarLagu();
+            // Daftar Lagu
+            break;
+        case 4: searchingLagu();
+            // Cari Lagu
+            break;
+        case 0: exit(0);
+            // Keluar
+            break;
+        default:
+            std::cout << "Pilihan tidak valid!\n";
+            break;
     }
-    
-  }
+}
+
 };
 
 // TODO: Buat Tampilan yang Cantik (gunain Text buat warna, bold, atau agak
