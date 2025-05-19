a2573aca8d60f80c1a4e4eaf617c770bcc5fd84a
RafidH777 <124240060@student.upnyk.ac.id>
2025-05-19 23:33:59 +0700

    membuat tampilan

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <vector>
 
 /*
  * @brief Class Text digunakan untuk memodifikasi atau memanipulasi
@@ -51,15 +53,92 @@ class ConsoleUI {
     std::cout << "-------------------------------------\n\n";
   }
 
-  static void lihatPlaylist(){
+  static void daftarPlaylist(){
     std::cout << "\n========================================\n";
     std::cout << "            DAFTAR PLAYLIST           \n";
     std::cout << "========================================\n";
+     std::cout << "\n" << Text::bold("Playlist yang Kamu Miliki:\n");
+    std::cout << std::setfill('=') << std::setw(40) << "=" << std::setfill(' ') << "\n";
+
+        std::cout << std::left
+          << std::setw(5) << Text::bold("No")
+          << std::setw(25) << Text::bold("Nama Playlist");
+    std::cout << std::setfill('-') << std::setw(40) << "-" << std::setfill(' ') << "\n";
+    const auto;
+        std::cout << std::left
+                  << std::setw(5) // nomor
+                  << std::setw(22) //nama playlist
+                  << "\n";
+    std::cout << "=================================================================\n";
+        std::cout << "\nPilih playlist yang ingin kamu kelola: ";
+    std::cout << "\n\n" << Text::underline("Pilihan Menu:");
+    std::cout << "\n1. Lihat isi playlist";
+    std::cout << "\n2. Tambah lagu ke playlist";
+    std::cout << "\n3. Hapus lagu dari playlist";
+    std::cout << "\n4. Hapus playlist";
+    std::cout << "\n5. Kembali ke menu utama\n";
+
+
   }
+
+  
+static void tabelLagu() {
+    std::cout << std::left
+          << std::setw(5)  << Text::bold("No")
+          << std::setw(22) << Text::bold("Judul")
+          << std::setw(22) << Text::bold("Artis")
+          << std::setw(8)  << Text::bold("Tahun")
+          << std::setw(12) << Text::bold("Diputar") << "\n";
+    std::cout << std::setfill('-') << std::setw(69) << "-" << std::setfill(' ') << "\n";
+    
+        const auto;
+        std::cout << std::left
+                  << std::setw(5) // nomor
+                  << std::setw(22) //judul
+                  << std::setw(22) //artis
+                  << std::setw(8) //tahun
+                  << std::setw(12) //diputar
+                  << "\n";
+    
+
+    std::cout << "=================================================================\n";
+  }
+  static void menuSorting(){
+    std::cout << "Mau Urutan Berdasarkan :\n";
+    std::cout << "1. Ascending\n";
+    std::cout << "2. Descending\n";
+    std::cout << "Pilihan: ";
+    int pilihan;
+    std::cin >> pilihan;
+  }
+
   static void daftarLagu(){
     std::cout << "\n===========================================\n";
     std::cout << "               DAFTAR LAGU               \n";
     std::cout << "===========================================\n";
+    std::cout << "\n Daftar Semua Lagu:\n";
+    tabelLagu(); 
+    std::cout << "\nIngin mengurutkan berdasarkan:\n";
+    std::cout << "1. Tahun\n";
+    std::cout << "2. Most Played\n";
+    std::cout << "3. Judul\n";
+    std::cout << "Pilihan: ";
+    int pilihan;
+    std::cin >> pilihan;
+    switch (pilihan)
+    {
+    case 1:
+      menuSorting();
+      break;
+    case 2:
+      menuSorting();
+      break;
+    case 3:
+      menuSorting();
+      break;
+    default: std::cout << "Pilihan tidak valid!\n";
+      break;
+    }
   }
 
   static void searchingLagu(){
@@ -92,7 +171,7 @@ class ConsoleUI {
         case 1: buatPlaylist();
             // Buat Playlist
             break;
-        case 2: lihatPlaylist();
+        case 2: daftarPlaylist();
             // Lihat Playlist
             break;
         case 3: daftarLagu();
